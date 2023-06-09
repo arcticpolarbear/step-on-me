278ee2fa8f2d4171ca378e902268d5985c5808e3
David Kamayor <davidkamayor@gmail.com>
2023-06-09 14:43:08 -0400

    implement hover effect for BeatSelector

--- src/components/coach/ui/BeatSelector.tsx
+++ src/components/coach/ui/BeatSelector.tsx
@@ -1,18 +1,17 @@
-import React from 'react'
+import React,{ useState } from 'react'
 import { BeatSelectorProps } from '../../../interfaces/CoachInterface';
 import './BeatSelector.css'
 
 //BeatSelector allows users to view steps on a selected beat.
 const BeatSelector:React.FC<BeatSelectorProps> = ({pattern, displayBeat}) => {
+    
     //might want to remove this check after a check is implemented in a future "updatePattern()"
     if (!pattern.beats || !pattern.steps || !pattern.signature) {
         // handleError('BeatSelector did not receive a valid dance pattern!');
         return(<p>'No valid dance pattern!'</p>);
     }
-    
-    
 
-    //generates child "div" elements for the parent element "beat-selector"
+    //generates child "button" elements for the parent element "beat-selector"
     //the first beat of each measure is marked
     //"signature" is the number of beats per measure
     function generateDivisions(beats: number, signature: number) {
@@ -20,12 +19,20 @@ const BeatSelector:React.FC<BeatSelectorProps> = ({pattern, displayBeat}) => {
         let firstBeat = 0
         for (let i = 0; i < beats; i++) {
             if (i === firstBeat) {
-                items.push(<button key={i}  className="first-beat" onClick={() => displayBeat(i)}>Beat {i + 1}</button>);
+                //button elements with event handlers below
+                items.push(
+                    <button key={`beat-${i}`} className={'beat first'} onClick={() => displayBeat(i)}>
+                            Beat {i + 1}
+                    </button>);
                 //moves firstBeat marker to next measure
                 firstBeat += signature;
                 continue;
             }
-            items.push(<button key={i} className="beat-division" onClick={() => displayBeat(i)}>Beat {i + 1}</button>);
+            //button elements with event handlers below
+            items.push(
+                <button key={`beat-${i}`} className={'beat'} onClick={() => displayBeat(i)}>
+                        Beat {i + 1}
+                </button>);
         }
         return items;
     }
@@ -33,7 +40,7 @@ const BeatSelector:React.FC<BeatSelectorProps> = ({pattern, displayBeat}) => {
     return(
     <div>
         <h1>This is the interactive Beat Selector!</h1>
-        <div className="beat-selector">{generateDivisions(pattern.beats, pattern.signature)}</div>
+        <div className="selector-bar">{generateDivisions(pattern.beats, pattern.signature)}</div>
     </div>
     )
 }
